c7c1a33e95c3d16378b3c591cd8a831000d40c03
RobCrawford527 <rcrawford527@hotmail.co.uk>
2022-12-01 18:01:59 +0000

    Added documentation for deltaR

--- R/deltaR.R
+++ R/deltaR.R
@@ -1,27 +1,42 @@
+#' Calculate Log2 Differences Between Samples
+#'
+#' @param data A data frame in long format. The conditions you wish to
+#'     compare must be present in a separate column to the other sample
+#'     information.
+#' @param val_col Column containing intensity values
+#' @param key_col Column containing the conditions to compare
+#' @param delta Vector specifying the conditions to compare (default is all)
+#'
+#' @return A data frame containing log2 differences for the comparisons of
+#'     interest
+#' @export
+#'
+#' @examples
+#'
 deltaR <- function(data,
                    val_col,
                    key_col,
                    delta = NULL){
-  
+
   # change column names
   colnames(data)[colnames(data) == val_col] <- "val"
   colnames(data)[colnames(data) == key_col] <- "key"
-  
+
   # define levels of factor of interest
   if (is.null(delta)){
     delta <- unique(data$key)
   }
-  
+
   # spread data using key column
   data <- tidyr::spread(data = data,
                         key = key,
                         value = val)
-  
+
   # create vector to store comparison names
   # set index
   comparisons <- vector(mode = "character")
   index <- 1
-  
+
   # perform pairwise comparisons
   # only in one direction
   for (i in 1:(length(delta)-1)){
@@ -29,7 +44,7 @@ deltaR <- function(data,
       if (i < j){
         # calculate deltaR
         data[,"delta"] <- data[,delta[j]] - data[,delta[i]]
-        
+
         # rename delta column
         # write comparison into vector
         # iterate index
@@ -39,7 +54,7 @@ deltaR <- function(data,
       }
     }
   }
-  
+
   # keep comparison columns only
   # gather by comparison
   # create new column to separate sample and reference
@@ -48,7 +63,7 @@ deltaR <- function(data,
                         key = "comparison",
                         value = "deltaR",
                         dplyr::all_of(comparisons))
-  
+
   # return output data frame
   data
 }
