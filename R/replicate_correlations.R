7ac904ef8cc8209ce94c824f6a949150f930609b
RobCrawford527 <rcrawford527@hotmail.co.uk>
2023-03-10 11:56:49 +0000

    Updates to add dplyr functionality to replicate_correlations.

--- R/replicate_correlations.R
+++ R/replicate_correlations.R
@@ -2,9 +2,10 @@
 #'
 #' @param data A data frame in long format
 #' @param samples List of samples to assess (defaults to all)
-#' @param sam_col Column containing samples
-#' @param val_col Column containing intensity values
-#' @param rep_col Column containing replicates
+#' @param protein_id Column containing protein names
+#' @param sample Column containing samples
+#' @param value Column containing intensity values
+#' @param replicate Column containing replicates
 #' @param nrow Number of rows for facet_wrap
 #' @param ncol Number of columns for facet_wrap
 #' @param limits Limits for colour scale
@@ -17,89 +18,79 @@
 #'
 replicate_correlations <- function(data,
                                    samples = NULL,
-                                   names_col,
-                                   sam_col,
-                                   val_col,
-                                   rep_col,
+                                   protein_id,
+                                   sample,
+                                   value,
+                                   replicate,
                                    nrow = NULL,
                                    ncol = NULL,
                                    limits = NULL){
 
-  # change column names
-  colnames(data)[colnames(data) == names_col] <- "name"
-  colnames(data)[colnames(data) == sam_col] <- "sam"
-  colnames(data)[colnames(data) == val_col] <- "val"
-  colnames(data)[colnames(data) == rep_col] <- "rep"
-
-  # create samples if not defined already
+  # create list of all samples if not defined already
   if (is.null(samples)){
-    samples <- unique(data$sam)
+    samples <- unique(data[[sample]])
   }
 
   # define replicates
-  replicates <- levels(as.factor(data$rep))
+  replicates <- unique(data[[replicate]])
 
   # spread data
-  data <- tidyr::spread(data = data[,c("name", "sam", "rep", "val")],
-                        key = rep,
-                        value = val)
+  data <- tidyr::spread(data = data[,c( {{ protein_id }}, {{ sample }}, {{ replicate }}, {{ value}} )],
+                        key = {{ replicate }},
+                        value = {{ value }} )
 
   # create output data frame
-  correlations <- data.frame()
-  for (s in samples){
-    for (r in replicates){
-      correlations <- rbind.data.frame(correlations,
-                                       data.frame(sample = s,
-                                                  RepX = r,
-                                                  RepY = replicates,
-                                                  adjrsq = NA,
-                                                  shared = NA))
-    }
-  }
-  correlations <- dplyr::filter(correlations, RepX < RepY)
+  correlations <- tidyr::expand_grid(samples, replicates, replicates)
+  rownames(correlations) <- c("sample", "rep_x", "rep_y")
+  correlations <- dplyr::filter(correlations,
+                                rep_x < rep_y)
+  correlations <- dplyr::mutate(correlations,
+                                adj_rsq = NA,
+                                shared = NA)
 
   # calculate correlations between replicates
   for (i in 1:nrow(correlations)){
 
     # determine parameters for row of interest
     s <- correlations[i, "sample"]
-    rx <- as.character(correlations[i, "RepX"])
-    ry <- as.character(correlations[i, "RepY"])
+    rx <- as.character(correlations[i, "rep_x"])
+    ry <- as.character(correlations[i, "rep_y"])
 
     # filter data
-    data_i <- dplyr::filter(data, sam == s)
-    data_i <- na.omit(data_i[,c(rx, ry)])
+    data_i <- dplyr::filter(data,
+                            sample == s)
+    data_i <- na.omit(data_i)
     colnames(data_i) <- c("rx", "ry")
 
     if (nrow(data_i) > 0){
       # calculate adjusted r-squared
       # write into results table
-      correlations[i, "adjrsq"] <- summary(stats::lm(ry ~ rx, data = data_i))["adj.r.squared"]
+      correlations[i, "adj_rsq"] <- summary(stats::lm( {{ ry }} ~ {{ rx }} , data_i))["adj.r.squared"]
       correlations[i, "shared"] <- nrow(data_i)
 
     } else if (nrow(data_i) == 0){
       # write into results table
-      correlations[i, "adjrsq"] <- NA
+      correlations[i, "adj_rsq"] <- NA
       correlations[i, "shared"] <- 0
     }
   }
 
   # format output data frame
-  correlations[,"RepX"] <- as.factor(correlations[,"RepX"])
-  correlations[,"RepY"] <- as.factor(correlations[,"RepY"])
+  correlations[,"rep_x"] <- as.factor(correlations[,"rep_x"])
+  correlations[,"rep_y"] <- as.factor(correlations[,"rep_y"])
 
   # define scale limits if not already
   if (is.null(limits)){
-    limits <- c(round(min(correlations$adjrsq)-0.05, 1), 1.0)
+    limits <- c(round(min(correlations[["adj_rsq"]]) - 0.05, 1), 1.0)
   }
 
   # plot correlations
   plot <- ggplot2::ggplot(data = correlations,
-                          mapping = ggplot2::aes(x = RepX,
-                                                 y = RepY,
-                                                 fill = adjrsq)) +
+                          mapping = ggplot2::aes(x = rep_x,
+                                                 y = rep_y,
+                                                 fill = adj_rsq)) +
     ggplot2::geom_tile() +
-    ggplot2::geom_text(mapping = ggplot2::aes(label = signif(adjrsq,2),
+    ggplot2::geom_text(mapping = ggplot2::aes(label = signif(adj_rsq, 2),
                                               fill = NULL),
                        nudge_y = 0.15,
                        colour = "white") +
