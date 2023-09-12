be8c1259b839e3c9947168ad547e08266499f0f1
RobCrawford527 <rcrawford527@hotmail.co.uk>
2023-09-12 14:03:47 +0100

    New version of assign_missingness

--- R/assign_missingness.R
+++ R/assign_missingness.R
@@ -1,118 +1,78 @@
 #' Assign Missingness to Proteomics Samples
 #'
 #' @param data A data frame in long format
-#' @param names_col Column containing protein names
-#' @param sam_col Column containing samples
-#' @param val_col Column containing intensity values
-#' @param rep_col Column containing replicates
-#' @param ref Reference sample
+#' @param name Column containing protein names
+#' @param sample Column containing samples
+#' @param experiment Column containing experiments
+#' @param replicate Column containing replicates
+#' @param value Column containing values
+#' @param reference Reference sample
 #' @param MAR_thres Minimum samples for MAR (default 2)
 #' @param MNAR_thres Maximum samples for MNAR (default 0)
 #'
-#' @return A data frame in long format, with missingness assigned for
-#'     each comparison
+#' @return A data frame in long format, with missingness assigned for each comparison
 #' @export
 #'
-#' @examples
-#'
 assign_missingness <- function(data,
-                               names_col,
-                               sam_col,
-                               val_col,
-                               rep_col,
-                               ref,
+                               name,
+                               sample,
+                               experiment,
+                               replicate,
+                               value,
+                               reference,
                                MAR_thres = 2,
                                MNAR_thres = 0){
 
-  # change column names
-  colnames(data)[colnames(data) == names_col] <- "names"
-  colnames(data)[colnames(data) == sam_col] <- "sam"
-  colnames(data)[colnames(data) == val_col] <- "val"
-  colnames(data)[colnames(data) == rep_col] <- "rep"
-
   # define replicates
   # define total number of replicates
-  replicates <- levels(as.factor(data$rep))
+  data <- dplyr::ungroup(data)
+  replicates <- dplyr::select(data, {{ replicate }} )
+  replicates <- unlist(unique(replicates))
   full <- length(replicates)
 
   # create copy of data
   # count number of replicates each protein found in
-  count <- data
-  count <- tidyr::spread(data = count,
-                         key = rep,
-                         value = val)
-  count$count <- rowSums(!is.na(count[,replicates]))
+  count <- dplyr::group_by(data, {{ name }} , {{ sample }} )
+  count <- dplyr::summarise(count,
+                            n = dplyr::n(),
+                            .groups = "drop")
 
-  # keep only count column
-  # spread using sample column
-  count <- count[,colnames(count)[!(colnames(count) %in% replicates)]]
-  samples <- unique(count$sam)
-  count <- tidyr::spread(data = count,
-                         key = sam,
-                         value = count)
+  count <- dplyr::group_by(count, {{ name }} )
+  count_ref <- dplyr::filter(count, {{ sample }} == reference)
+  count_ref <- dplyr::select(count_ref, {{ name }}, ref_n = n)
 
-  # create vector to store comparison names
-  comparisons <- vector(mode = "character")
+  count <- dplyr::left_join(count, count_ref, by = dplyr::join_by( {{ name }} == {{ name }} ))
 
-  # determine missingness
-  for (sample_of_interest in samples[samples != ref]){
+  # assign missingness for each protein
+  count <- dplyr::mutate(count,
+                         comparison = paste( {{ sample }} , reference, sep = "_vs_"),
+                         missingness = dplyr::case_when(n == full & ref_n == full ~ "complete",
+                                                        (n < MAR_thres & ref_n < MAR_thres) | (n != full & is.na(ref_n)) ~ NA,
+                                                        (((n >= MAR_thres & ref_n >= MAR_thres) & (n + ref_n >= full)) | (n == full & ref_n > MNAR_thres) | (n > MNAR_thres & ref_n == full)) ~ "MAR",
+                                                        ((n == full & ref_n <= MNAR_thres) | (n <= MNAR_thres & ref_n == full) | (n == full & is.na(ref_n))) ~ "MNAR",
+                                                        TRUE ~ NA))
 
-    # rename columns
-    colnames(count)[colnames(count) == sample_of_interest] <- "s"
-    colnames(count)[colnames(count) == ref] <- "ref"
+  # join missingness assingments into original data frame
+  output <- dplyr::left_join(data, count, by = dplyr::join_by( {{ name }} == {{ name }} ,
+                                                               {{ sample }} == {{ sample }} ))
 
-    # create new column with outcome of missingness assignment
-    count <- dplyr::mutate(.data = count,
-                           missingness = dplyr::case_when(s == full & ref == full ~ "complete",
-                                                          s < MAR_thres & ref < MAR_thres ~ "missing",
-                                                          ((s >= MAR_thres & ref >= MAR_thres) & (s + ref >= full)) ~ "MAR",
-                                                          ((s == full & ref > MNAR_thres) | (s > MNAR_thres & ref == full)) ~ "MAR",
-                                                          ((s == full & ref <= MNAR_thres) | (s <= MNAR_thres & ref == full)) ~ "MNAR"))
 
-    # revert columns to original names
-    # rename missingness column as comparison
-    colnames(count)[colnames(count) == "s"] <- sample_of_interest
-    colnames(count)[colnames(count) == "missingness"] <- paste(sample_of_interest, "vs", ref, sep="_")
-    colnames(count)[colnames(count) == "ref"] <- ref
+  # reference data
+  comparisons <- dplyr::filter(output, {{ sample }} != reference)
+  comparisons <- dplyr::select(comparisons,
+                               -c( {{ sample }}, {{ experiment }}, {{ replicate }}, {{ value }}, n))
 
-    # write comparison into vector
-    comparisons[sample_of_interest] <- paste(sample_of_interest, "vs", ref, sep="_")
-  }
+  reference_data <- dplyr::filter(output, {{ sample }} == reference)
+  reference_data <- dplyr::select(reference_data,
+                                  {{ name }}, {{ sample }}, {{ experiment }}, {{ replicate }}, {{ value }}, n)
 
-  # keep comparison columns only
-  # gather by comparison
-  # create new column to separate sample and reference
-  count <- count[,colnames(count)[!(colnames(count) %in% samples)]]
-  count <- tidyr::gather(data = count,
-                         key = "comparison",
-                         value = "missingness",
-                         dplyr::all_of(comparisons))
-  count[,"sample"] <- count[,"comparison"]
-  count <- tidyr::separate(data = count,
-                           col = sample,
-                           into = c("sample", "reference"),
-                           sep = "_vs_")
-  count <- tidyr::gather(data = count,
-                         key = "type",
-                         value = "sam",
-                         c("sample", "reference"))
-  count <- count[,c("names", "comparison", "missingness", "type", "sam")]
+  reference_data <- dplyr::left_join(comparisons, reference_data, by = dplyr::join_by( {{ name }} == {{ name }} ), relationship = "many-to-many")
+  reference_data <- dplyr::filter(reference_data, !is.na( {{ sample }} ))
 
-  # merge original data with missingness data frame
-  # remove comparisons where all values are missing
-  data <- merge(data,
-                count,
-                by = c("names", "sam"),
-                all.x = TRUE)
-  data <- dplyr::filter(.data = data,
-                        missingness != "missing")
 
-  # revert column names
-  colnames(data)[colnames(data) == "names"] <- names_col
-  colnames(data)[colnames(data) == "sam"] <- sam_col
-  colnames(data)[colnames(data) == "rep"] <- rep_col
-  colnames(data)[colnames(data) == "val"] <- val_col
+  output <- dplyr::filter(output, {{ sample }} != reference)
+  output <- dplyr::bind_rows(output, reference_data)
 
   # return output data frame
-  data
+  output
 }
